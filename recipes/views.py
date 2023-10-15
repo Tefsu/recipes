2b9e43f972eae12010c17886ebb4a54cb70b45b6
Tefsu <tefersonluan@gmail.com>
2023-10-15 19:01:02 -0300

    injetando dados nas views

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     # Return HTTP Response
     # return HttpResponse('Home recipes')
     return render(request, 'recipes/pages/home.html', status=200, context={
-        'name': 'Teferson Luan'
+        'recipes': [make_recipe() for _ in range(10)]
     }) # Recomendado uso de namespace para evitar colisões e confusões no codigo
 
 def recipe(request, id):
-    # Return HTTP Response
-    # return HttpResponse('Home recipes')
     return render(request, 'recipes/pages/recipe-view.html', status=200, context={
-        'name': 'Teferson Luan'
+        'recipe': make_recipe()
     }) # Recomendado uso de namespace para evitar colisões e confusões no codigo
