9609d07117cb345cdc15e3cc5a5a10be8dd2c9bc
Tefsu <tefersonluan@gmail.com>
2023-10-15 16:47:46 -0300

    urls and separete templates

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from recipes.views import home
+# from recipes.views import home
+from . import views # metodo para não ter que ir importando view por view na mão
 
 urlpatterns = [
-    path('', home), # Home
+    path('', views.home), # Home
+    path('recipes/<int:id>/', views.recipe), 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,10 @@ def home(request):
     return render(request, 'recipes/pages/home.html', status=200, context={
         'name': 'Teferson Luan'
     }) # Recomendado uso de namespace para evitar colisões e confusões no codigo
+
+def recipe(request, id):
+    # Return HTTP Response
+    # return HttpResponse('Home recipes')
+    return render(request, 'recipes/pages/recipe-view.html', status=200, context={
+        'name': 'Teferson Luan'
+    }) # Recomendado uso de namespace para evitar colisões e confusões no codigo
