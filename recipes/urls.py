f649262e18804762fcad8478dd4cb7a2e971accf
Tefsu <tefersonluan@gmail.com>
2023-10-16 21:38:16 -0300

    nome unico na url e app_name para namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 # from recipes.views import home
 from . import views # metodo para não ter que ir importando view por view na mão
 
+# name space para urls 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), # Home
-    path('recipes/<int:id>/', views.recipe), 
+    path('', views.home, name='home'), # Home
+    path('recipes/<int:id>/', views.recipe, name='recipe'), 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -12,5 +12,6 @@ def home(request):
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', status=200, context={
-        'recipe': make_recipe()
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     }) # Recomendado uso de namespace para evitar colisões e confusões no codigo
