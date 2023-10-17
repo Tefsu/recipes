b1f8e557e0d63dff783e71e557eb38db299f4808
Tefsu <tefersonluan@gmail.com>
2023-10-16 22:13:00 -0300

    models e migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
+# modelos representão uma tabela do banco de dados e são atualizados conforme vc faz migrações
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d/")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
